c0a7e07d154a4ba68488dbc2f265f35a81348a82
MaryGonzalez <mgonzalez4231@gmail.com>
2019-10-21 20:33:41 -0500

    Update contactForm.php

--- Email/contactForm.php
+++ Email/contactForm.php
@@ -4,20 +4,18 @@
     $message = $Post['message'];
 
     $email_form = 'mgonzalez4231@gmail.com';
-
     $email_subject = "New Form Submission";
-
     $email_body ="User Name: $name. \n" . "User Email: $email. \n" . 
         "User Message: $message. \n";
-
     $to = "mgonzalez4231@gmail.com";
-
     $headers = "From: $email_form \r\n";
-
     $headers .= "Reply-to: $email \r\n";
 
-    mail($to, $email, $email_body, $headers);
+    if(mail($to, $email_subject, $email_body, $headers)){
+       header("Location: thankYou.html");
+   }else{
+       header("Location: error.html");
+   }
 
-    header("Location: index.html");
 
 ?>
